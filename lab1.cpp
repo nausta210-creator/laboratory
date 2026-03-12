fbfc4bf5b1a81acdc29ff05f9bd77b22a4e60e91
nausta210-creator <nausta210@gmail.com>
2026-03-12 20:52:03 +0700

    Добавлен lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+ 
+int main(){
+    float a, b, c;
+
+    cout<<"Введите длину первой стороны треугольника: ";
+    cin>>a;
+    cout<<"Введите длину второй стороны треугольника: ";
+    cin>>b;
+    cout<<"Введите длину третьей стороны треугольника: ";
+    cin>>c;
+
+    if (a+b>c && b+c>a && c+a>b){
+        float perimetr=a+b+c;
+        
+        float p=perimetr/2;
+        float ploshad=sqrt(p*(p-a)*(p-b)*(p-c));
+        
+        cout<<"Периметр треугольника: "<<perimetr<<endl;
+        cout<<"Площадь треугольника: "<<ploshad<<endl;
+
+        if (a==b || b==c || a==c)
+        {
+            cout<<"Треугольник равнобедренный"<<endl;
+        }
+        else
+        {
+            cout<<"Треугольник не равнобедренный"<<endl;
+        }
+    }
+    else
+    {
+        cout<<"Такого треугольника не существует";
+    }
+    return 0;
+}
