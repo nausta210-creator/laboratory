bac126dd2bfc6d611074fc5242f00041a2e0764d
nausta210-creator <nausta210@gmsil.com>
2026-03-13 10:05:10 +0700

    Внесенны изменения

--- lab1.cpp
+++ lab1.cpp
@@ -12,28 +12,33 @@ int main(){
     cin>>b;
     cout<<"Введите длину третьей стороны треугольника: ";
     cin>>c;
+    if (a>0 && b>0 && c>0){
+        if (a + b > c && b + c > a && c + a >b){
+            float perimetr = a + b + c;
+            
+            float p = perimetr / 2;
+            float ploshad = sqrt(p*(p-a)*(p-b)*(p-c));
+            
+            cout<<"Периметр треугольника: " << perimetr <<endl;
+            cout<<"Площадь треугольника: " << ploshad <<endl;
 
-    if (a+b>c && b+c>a && c+a>b){
-        float perimetr=a+b+c;
-        
-        float p=perimetr/2;
-        float ploshad=sqrt(p*(p-a)*(p-b)*(p-c));
-        
-        cout<<"Периметр треугольника: "<<perimetr<<endl;
-        cout<<"Площадь треугольника: "<<ploshad<<endl;
-
-        if (a==b || b==c || a==c)
-        {
-            cout<<"Треугольник равнобедренный"<<endl;
+            if (a==b || b==c || a==c)
+            {
+                cout<<"Треугольник равнобедренный"<<endl;
+            }
+            else
+            {
+                cout<<"Треугольник не равнобедренный"<<endl;
+            }
         }
         else
         {
-            cout<<"Треугольник не равнобедренный"<<endl;
+            cout<<"Такого треугольника не существует";
         }
     }
     else
     {
-        cout<<"Такого треугольника не существует";
+        cout<<"Введены отрицательные значения!!!"<<endl;
     }
-    return 0;
+        return 0;
 }
