848aa265728df7d9dd1292de25210a1c94ed006d
SafronovaAnn <dolgovaannochka05@mail.ru>
2026-03-22 20:33:44 +0700

    Добавление программы для вычисления параметров окружности

---
+++ circle.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+const double PI = 3.14159265358979323846;
+ 
+//Функция вычисления длины окружности
+double circleLenght(double r){
+return 2*PI*r;
+}
+//Функция вычисления площади круга
+double circleArea(double r){
+return PI*r*r;
+}
+ 
+//Функция вычисления площади кругового сектора
+double sectorArea(double r, double angle){
+return (PI*r*r*angle)/360;
+}
+int main(){
+double r, angle;
+ 
+//Запросить радиус окружности
+cout<< "Вычисление параметров окружности"<<endl;
+cout<<"Введите радиус окружности: ";
+cin>>r;
+if (r<=0){
+cout <<"Радиус должен быть положительным и не равным нулю!"<<endl;
+return 1;
+}
+ 
+//Запростить угол сектора
+cout <<"Введите угол сектора в градусах: "<<endl;
+cin>>angle;
+ if (angle<0 || angle >360){
+cout <<"Угол должен быть от 0 до 360!" <<endl;
+return 1;
+}
+cout<<"Результат: "<< endl;
+cout<< "Длина окружности: "<< circleLenght(r)<<endl;
+cout<< "Площадь круга: "<< circleArea(r)<<endl;
+cout<< "Площадь сектора: "<< sectorArea(r, angle)<<endl;
+ 
+return 0;
+}
